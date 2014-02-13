6d84b87d7ad9ffb7cd05a504e9d4aa499a517c13
David Little <d-little@u.northwestern.edu>
2014-02-13 17:02:33 -0600

    - prior to 'ml' adapter added - Added dialog to query user - added order to line writier

--- blocked_audio.py
+++ blocked_audio.py
@@ -8,6 +8,7 @@ from psychopy.gui import DlgFromDict
 
 stimulus = {}
 env = {}
+run = True
 
 rhythm = 'A'
 stimulus = {'atten_dB': 10,
@@ -17,19 +18,21 @@ stimulus = {'atten_dB': 10,
             'SOA_ms': 900,
             'intervals': rhythm_intervals(rhythm)}
 
-alphas = np.linspace(0,0.3,num=4)
+def adapter():
+    alphas = np.linspace(0,0.3,num=4)
 
-slopes = (adapters.sig_solve_slope(p=0.95,x=delta,alpha=0.0) \
+    slopes = (adapters.sig_solve_slope(p=0.95,x=delta,alpha=0.0) \
           for delta in np.linspace(0.01,0.95,num=1000))
 
-ml_adapter = adapters.MLAdapter(0.95,sorted(slopes),alphas)
+    slope_sigma = adapters.sig_solve_slope(p=0.95,x=0.01,alpha=0.0)
+
+    return adapters.MLAdapter(0.95,sorted(slopes),alphas,slope_sigma)
 
 env = {'debug': False,
        'sample_rate_Hz': 44100,
        'data_file_dir': 'data',
-       'num_trials': 30,
-       'feedback_delay_ms': 500,
-       'adapter': ml_adapter}
+       'num_trials': 50,
+       'feedback_delay_ms': 500}
 
 def create_window(env):
     if env['debug']: return Window([400,400])
@@ -44,22 +47,28 @@ def blocked_audio(sid,group,phase,start_block,num_blocks):
         info['group'] = group
         info['phase'] = phase
         info['rhythm'] = rhythm
+        info_order = ['sid','group','phase','rhythm']
         
         TextStim(env['win'],text='Tapping Task')
         wait(3.0)
         
-        dfile = unique_file(env['data_file_dir'] + '/' + sid + '_' +
-                            time.strftime("%Y_%m_%d_") + phase + "_%02d.dat")
-
         for i in range(start_block,num_blocks):
+            env['adapter'] = adapter()
             info['block'] = i
-            audio.run(env,stimulus,LineWriter(dfile,info))
+            dfile = unique_file(env['data_file_dir'] + '/' + sid + '_' +
+                            time.strftime("%Y_%m_%d_") + phase + "_%02d.dat")
+            
+            audio.run(env,stimulus,LineWriter(dfile,info,info_order))
     finally:
         env['win'].close()
-        
-setup = {'User ID': '0000', 'Group': 'test', 'Phase': 'phase',
-         'Blocks': 5, 'Start Block': 0}
-dialog = DlgFromDict(dictionary=setup,title='Rhythm Discrimination')
-if dialog.OK:
-    blocked_audio(setup['User ID'],setup['Group'],setup['Phase'],
-                  setup['Start Block'],setup['Blocks'])
+
+
+
+if run:
+    setup = {'User ID': '0000', 'Group': 'pilot', 'Phase': 'test',
+            'Blocks': 5, 'Start Block': 0}
+    dialog = DlgFromDict(dictionary=setup,title='Rhythm Discrimination',
+                        order=['User ID','Group','Phase','Blocks','Start Block'])
+    if dialog.OK:
+        blocked_audio(setup['User ID'],setup['Group'],setup['Phase'],
+                      setup['Start Block'],setup['Blocks'])
