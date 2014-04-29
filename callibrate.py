e5ca0a3dcf0637a90e93674ae245605853a98003
unknown <dfl909@sccs10ba12.soc.northwestern.edu>
2014-04-29 12:06:59 -0500

    Fixed errors in calibrate program.

--- callibrate.py
+++ callibrate.py
@@ -1,12 +1,14 @@
 from psychopy.gui import DlgFromDict
+from util import *
+from psychopy.sound import Sound
+from psychopy.core import *
 
 setup = {'frequency': 1000,'attenuation': 0}
 dialog = DlgFromDict(dictionary=setup,title='Calibration',
             order=['frequency','attenuation'])
 if dialog.OK:
-    beep = tone(setup['frequency'],5000,setup['attenuation'],5,44100)
-
+    beep = left(tone(setup['frequency'],5000,setup['attenuation'],5,44100))
     sound = Sound(beep.copy())
     sound.play()
-                
+    wait(5,5)
                 
