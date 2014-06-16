e0f5e3f2895e2d5a769b1508e1f2fbd950b7dead
David Little <d-little@u.northwestern.edu>
2014-06-16 15:07:14 -0500

    Added function to replay stimuli from previosuly run active sesison.

--- passive.py
+++ passive.py
@@ -8,6 +8,7 @@ from util import tone, Info
 import random
 import numpy as np
 import datetime
+import pandas as pd
 
 def examples(env,stimulus):
     high_message = TextStim(env['win'],
@@ -78,5 +79,52 @@ def run(env,stimulus,write_line):
         delay = env['feedback_delay_ms'] / 1000.0
         wait(delay,delay)
         
+def run_track(env,stimulus,track,write_line):
+    
+    env['win'].flip()
+
+    wait(1,1)
+
+    delays_ms = pd.to_datetime(track.timestamp).diff()
+    # HACK!! 
+    delays_ms = delays_ms.apply(lambda x: x.astype('float64') / 1e6)
+    delays_ms[0] = delays_ms.median()
+
+    for track_index,track_row in track.iterrows():
+        signal_interval = track_row['correct_response']
+        if signal_interval == 0:
+            stim_1 = stimulus['generate'](track_row['delta'])
+            stim_2 = stimulus['generate'](0)
+            
+        else:
+            stim_1 = stimulus['generate'](0)
+            stim_2 = stimulus['generate'](track_row['delta'])
+        
+        stim_1.play()
+
+        delay = stimulus['SOA_ms']/1000.0 - stim_1.getDuration()
+        wait(delay,delay)
+        
+        stim_2.play()
+
+        delay = max((stimulus['response_delay_ms']/1000.0 - stim_2.getDuration()),
+                    delays_ms[track_index]/1000.0 - stim_2.getDuration() -
+                    stimulus['SOA_ms']/1000 - env['feedback_delay_ms']/1000.0)
+
+        wait(delay,delay)
+                
+        line_info = {'delta': track_row['delta'],
+                     'signal_interval': signal_interval,
+                     'timestamp': datetime.datetime.now()}
+
+        order = ['delta','signal_interval','timestamp']
+        
+        write_line(line_info,order)
+
+        delay = env['feedback_delay_ms'] / 1000.0
+        wait(delay,delay)
+        
+    
+    
     
     
