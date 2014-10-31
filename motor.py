b076b76a46c9f7fc304a0514487c0ae50fc1c6d1
David Little <d-little@u.northwestern.edu>
2014-10-31 15:25:55 -0500

    Motor sycnhronization task appears to be working now.

--- motor.py
+++ motor.py
@@ -1,11 +1,10 @@
-import expyriment
+import expyriment as ex
 from util import tone, Info
 import numpy as np
-import Queue
 import pygame
+import datetime
+import rtmidi
 from phase import phase
-
-use_response_pad = False
 
 KEY_UP_CODE = 128
 KEY_DOWN_CODE = 144
@@ -15,9 +14,15 @@ NANO_TRIGGER = 36 #(far left, bottom button)
 @phase('motor_synch')
 def run_blocks(env,stimulus,condition,block,is_start,write_line):
     if is_start:
-        ex.stimuli.TextBox(stimulus['instructions']).present()
-        env['exp'].keyobard.wait()
-    run(env,stimulus,write_line)
+        ex.stimuli.TextBox(stimulus['instructions'],(400,400)).present()
+        env['exp'].keyboard.wait()
+
+        ex.stimuli.TextLine('Loading...').present()
+        # pregenerate stimuli (results will be cached for future use
+        stimulus['generate'](stimulus['n_synch'])
+        env['exp'].screen.clear()
+
+    run(env,stimulus,block,write_line)
 
 # keyboard responder (used for debugging purposes)
 class KeyboardResponder:
@@ -26,11 +31,17 @@ class KeyboardResponder:
         self.exp = exp
     def __enter__(self):
         self.exp.keyboard.clear()
+        waiter = ex.misc.Clock()
         key = self.exp.keyboard.wait()
-        while key is self.key:
+        while key is not self.key:
             key = self.exp.keyboard.wait()
+            waiter.wait(10)
 
         return ResponseCollector(self)
+
+    def _translate_key(self,key):
+        return [[KEY_DOWN_CODE,key,0],0.5]
+
     def collect_messages(self):
         keys = []
         key = self.exp.keyboard.check(self.key)
@@ -38,7 +49,8 @@ class KeyboardResponder:
             keys.append(key)
             key = self.exp.keyboard.check(self.key)
 
-        return keys
+        return map(self._translate_key,keys)
+
     def __exit__(self,type,value,traceback):
         pass
 
@@ -55,16 +67,18 @@ class NanopadResponder:
         #self.messages = Queue.Queue(4096)
         #self.dev.set_callback(lambda m,_: self._save_message(m))
 
+        waiter = ex.misc.Clock()
         message = self.dev.get_message()
-        while message is None or message == self.key:
+        while message is None or message[0][1] != self.key:
             message = self.dev.get_message()
+            waiter.wait(10)
 
         return ResponseCollector(self)
 
     #def _save_message(self,message):
     #    if self.listening and self.key == message[0][1]: self.messages.put(message)
 
-    def collect_messages():
+    def collect_messages(self):
         # collect all queued events
         # messages = []
         # try:
@@ -77,21 +91,20 @@ class NanopadResponder:
         messages = []
         message = self.dev.get_message()
         while message is not None:
-            messaged.append(message)
+            messages.append(message)
             message = self.dev.get_message()
 
         return messages
 
     def __exit__(self,type,value,traceback):
         self.dev.close_port()
-        del self.messages
 
 class ResponseCollector:
     def __init__(self,responder):
         self.responder = responder
         self.last_collected = 0
         self.last_event = 0
-        self.clock = expyriment.misc.Clock()
+        self.clock = ex.misc.Clock()
         self.clock.reset_stopwatch()
                 
     def collect_events(self,until_ms=0):
@@ -115,49 +128,68 @@ class ResponseCollector:
             times = times[(codes == KEY_DOWN_CODE)] + event_offset
             strengths = strengths[(codes == KEY_DOWN_CODE)]
 
-            return times,strengths
+            return zip(times,strengths)
         else:
-            return [],[]
+            return []
 
 def collect_events(time,cycles):
-    with NanoPadResponder() as r:
+    with NanopadResponder() as r:
         for index in range(cycles):
             print "--------------------"
             times,strengths = r.collect_events(time)
             print index
             print times
 
-def run(env,stimulus,write_line):
-    # TODO: use another key to start each new trial
-    if use_response_pad:
+def run(env,stimulus,block,write_line):
+    env['exp'].screen.clear()
+
+    if env['use_response_pad']:
         start_message = \
-            ex.stimuli.TextBox('Press anything but the bottom left button to start a block.',(400,400))
+            ex.stimuli.TextBox('Press bottom left button to begin.',(400,400))                                       
     else:
         start_message = \
-          ex.stimuli.TextBox('Press anything but space bar to start a block.',(400,400))
-    
+          ex.stimuli.TextBox('Press spacebar to begin.',(400,400))
+          
+    countdown = [ex.stimuli.TextLine(str(i)) for i in
+                 range(env['countdown_length'],0,-1)]
+    go_text = ex.stimuli.TextLine('Go!')
+    for c in countdown: c.preload()
+    go_text.preload()
+
+    env['exp'].screen.clear()
     rhythm = stimulus['generate'](stimulus['n_synch'])
     
-    if use_response_pad:
-        responder = NanpadResponder()
+    if env['use_response_pad']:
+        responder = NanopadResponder()
     else:
-        responder = KeyboardResponder()
-    
-    for block in range(env['num_blocks']):
-        start_message.present()
-        listen_time = rhythm.get_length()*1000 + stimulus['continuation_ms']
-        
+        responder = KeyboardResponder(env['exp'])
+
+    start_message.present()
+    with responder as r:
+        # note the time
         timestamp = datetime.datetime.now()
-        # TODO: wait for user to indicate they're starting
+
+        # give a countdown
+        for c in countdown:
+            c.present()
+            env['exp'].clock.wait(env['countdown_interval'])
+        go_text.present()
+
+        # start playing the sound
+        listen_time = rhythm.get_length()*1000 + stimulus['continuation_ms']
+        rhythm.play()
+
+        # keep the go_text up for a while
+        env['exp'].clock.wait(500)
         env['exp'].screen.clear()
-        with responder:
-            rhtyhm.play()
-            
-            for i in range(np.ceil(listen_time / 500.0)):
-                for time,pressure in responder.collect_events(500):
-                    write_line({'block': block,
-                                'time': time,
-                                'pressure': pressure,
-                                'block_timestamp': timestamp},
-                                ['block','time','pressure','block_timestamp'])
+        env['exp'].screen.update()
+
+        # record all user taps to a file
+        for i in range(int(np.ceil(listen_time / 500.0))):
+            for time,pressure in r.collect_events(500):
+                write_line({'block': block,
+                            'time': time,
+                            'pressure': pressure,
+                            'block_timestamp': timestamp},
+                            ['block','time','pressure','block_timestamp'])
     
--- run_motor.py
+++ run_motor.py
@@ -11,10 +11,13 @@ import passive
 phases = ['motor_synch']
 
 env = {'title': 'Frequency Discrimination',
-       'debug': False,
+       'use_response_pad': True,
+       'countdown_interval': 750,
+       'countdown_length': 3,
+       'debug': True,
        'sample_rate_Hz': 44100,
        'groups': ['Test'],
-       'default_blocks': 6,
+       'default_blocks': 1,
        'data_file_dir': '../data',
        'num_trials': 60,
        'feedback_delay_ms': 400}
@@ -24,10 +27,11 @@ stimulus = {'atten_dB': 20, # TODO: calibration
             'freq_Hz': 250,
             'beep_ms': 25,
             'ramp_ms': 10,
+            'instructions': 'Tap along to the rhythm you hear. Continue to repeat the rhythm once the sound stops. ',
             'intervals': rhythm_intervals(rhythm),
-            'continuation_ms': 4700, #47000,
-            'n_synch': 1 #6
-            }
+            'continuation_ms': 4700,#47000,
+            'n_synch': 1,#6,
+            'condition_order': ['rhtyhm A']}
 
 def generate_tones(env,stimulus,condition,repeat):
     beep = tone(stimulus['freq_Hz'],stimulus['beep_ms'],
@@ -36,9 +40,9 @@ def generate_tones(env,stimulus,condition,repeat):
     sound = beep.copy()
     for cycle in range(stimulus['n_synch']):
         for interval in stimulus['intervals']:
-            sound = np.vstack([silence(interval,env['sample_rate_Hz']),
+            sound = np.vstack([sound, silence(interval,env['sample_rate_Hz']),
                                beep.copy()])
     return sound
-env['generate_tones'] = generate_tones
+stimulus['generate_tones'] = generate_tones
 
 experiment.start(env,stimulus,phases)
