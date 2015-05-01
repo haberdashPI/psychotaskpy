09be5e950883a0ef6e8294893174e7c2b5ba5bd3
David Little <david.frank.little@gmail.com>
2015-05-01 11:38:45 -0500

    Slight modifications to calibration.

--- callibrate.py
+++ callibrate.py
@@ -2,6 +2,7 @@ from util import *
 import pygame
 import pygame.mixer as mix
 import sys
+import time
 
 if len(sys.argv) >= 4:
     atten = int(sys.argv[1])
@@ -9,11 +10,12 @@ if len(sys.argv) >= 4:
     length = int(sys.argv[3])
     side = sys.argv[3]
 else:
-    atten = 20
+    atten = 30.4
     freq = 1000
-    length = 1000
+    length = 5000
     side = 'left'
 
+mix.pre_init(channels=2)
 pygame.init()
 
 if side == 'left':
@@ -22,3 +24,6 @@ else:
     floats = right(tone(freq,length,atten,5,44100))
 sound = mix.Sound(np.asarray(floats*(2**15),'int16'))
 sound.play()
+time.sleep(length/1000)
+# left 31.1
+# right 30.4
