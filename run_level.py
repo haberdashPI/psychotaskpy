2ff4f1ae64c86f0f0962b43ce9c306a4821cfc58
Bobby <robert.baudo@gmail.com>
2015-05-02 15:38:28 -0500

    Added calibration curve, next use it.

--- run_level.py
+++ run_level.py
@@ -12,9 +12,16 @@ import passive
 
 phases = ['2AFC']
 
-booth_atten = {'corner': 27.6, 'left': 25.7, # calibrated on 9-15-14
-               'middle': 30.7, # calibrated on 10-14-14
-               'none': 26}
+#booth_atten = {'corner': 27.6, 'left': 25.7, # calibrated on 9-15-14
+#               'middle': 30.7, # calibrated on 10-14-14
+#			   'right': 31.1, # calibrated on 04-15-15
+#               'none': 26}
+			   
+ # calibrated on 05-02-15			   
+calibration_curve = {'left': [(20,98.8),(40,78.8),(45,73.8),(50,68.75),(55,63.7),(60,58.6),
+							  (65,53.6),(70,48.5),(75,43.4),(80,37.7),(85,30.8),
+							  (87.5,26.9),(90,25.3),(91.25,22.6),(92.5,18),(93.75,13.6),(95,13)],
+					 'none': zip(np.linrange(20,80,10),np.linrange(100,10,10))}
 
 atten = booth_atten[booth()]
 print "Using attenuation of ",atten
@@ -50,7 +57,7 @@ def generate_sound(env,stimulus,condition,delta):
 
         beep = tone(cond,
                     stimulus['beep_ms'],
-                    stimulus['atten_dB'] - delta,
+                    stimulus['atten_dB'] + delta,
                     stimulus['ramp_ms'],
                     env['sample_rate_Hz'])
 
@@ -61,14 +68,14 @@ def generate_sound(env,stimulus,condition,delta):
 stimulus['generate_sound'] = generate_sound
 
 def generate_adapter(env,stimulus,condition):
-    params = pd.DataFrame({'theta': np.tile(np.linspace(-200,0,100),50),
+    params = pd.DataFrame({'theta': np.tile(np.linspace(120,-10,130),50),
                            'sigma': np.repeat(np.linspace(0.2,100,50),100),
                            'miss': 0.08})
 
     params['lp'] = np.log(scipy.stats.norm.pdf(params.theta,loc=0,scale=50)) + \
         np.log(scipy.stats.norm.pdf(params.sigma, loc=0,scale=50))
 
-    return adapters.KTAdapter(-10,np.linspace(-200,0,100),params)
+    return adapters.KTAdapter(-10,np.linspace(90,-30,100),params)
 
 env['generate_adapter'] = generate_adapter
 
